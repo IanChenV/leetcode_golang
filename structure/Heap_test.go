e7ee832452bb475458d0a0a4d21d66d72c726414
chenkeng45@gmail.com <chenkeng45@gmail.com>
2022-03-01 17:45:43 +0800

    up

--- structure/Heap_test.go
+++ structure/Heap_test.go
@@ -2,6 +2,7 @@ package structures
 
 import (
 	"container/heap"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -22,5 +23,10 @@ func Test_intHeap(t *testing.T) {
 	for i := begin; i < end; i++ {
 		heap.Push(ih, i)
 		ast.Equal(0, (*ih)[0], "插入 %d 后的最小值却是 %d，ih=%v", i, (*ih)[0], (*ih))
+	}
+
+	for i := begin; i < end; i++ {
+		fmt.Println(i, *ih)
+		ast.Equal(i, heap.Pop(ih), "Pop 后 ih=%v", (*ih))
 	}
 }
