8761eef360c753106a0016b60ad997a1febe9e8a
chenkeng45@gmail.com <chenkeng45@gmail.com>
2022-03-01 15:10:23 +0800

    queue

--- structure/Queue.go
+++ structure/Queue.go
@@ -3,3 +3,25 @@ package structure
 type Queue struct {
 	nums []int
 }
+
+func NewQueue() *Queue {
+	return &Queue{nums: []int{}}
+}
+
+func (q *Queue) Pop() int {
+	res := q.nums[0]
+	q.nums = q.nums[1:]
+	return res
+}
+
+func (q *Queue) Push(n int) {
+	q.nums = append(q.nums, n)
+}
+
+func (q *Queue) Len() int {
+	return len(q.nums)
+}
+
+func (q *Queue) IsEmpty() bool {
+	return q.Len() == 0
+}
