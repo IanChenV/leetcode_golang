8a7db600704158b90e16f892271d5c64b47f1679
chenkeng45@gmail.com <chenkeng45@gmail.com>
2022-03-01 19:24:43 +0800

    up

--- structure/Point.go
+++ structure/Point.go
@@ -3,3 +3,22 @@ package structure
 type Point struct {
 	X, Y int
 }
+
+func Intss2Points(points [][]int) []Point {
+	res := make([]Point, len(points))
+	for i, p := range points {
+		res[i] = Point{
+			X: p[0],
+			Y: p[1],
+		}
+	}
+	return res
+}
+
+func Point2Intss(points []Point) [][]int {
+	res := make([][]int, len(points))
+	for i, p := range points {
+		res[i] = []int{p.X, p.Y}
+	}
+	return res
+}
