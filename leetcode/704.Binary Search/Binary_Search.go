324bf801bf73eee6699744d4f95a921ec8c4f383
chenkeng45@gmail.com <chenkeng45@gmail.com>
2022-03-02 11:57:48 +0800

    up

---
+++ leetcode/704.Binary Search/Binary_Search.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func binarySearch(nums []int, target int) int {
+	low, high := 0, len(nums)-1
+	for low <= high {
+		mid := low + (high-low)>>1
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			high = mid - 1
+		} else if nums[mid] < target {
+			low = mid + 1
+		}
+	}
+	return -1
+}
