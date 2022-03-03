3929b69cbf2cb31a751d82c79a92d85cb52ce8e4
chenkeng45@gmail.com <chenkeng45@gmail.com>
2022-03-03 17:50:58 +0800

    up

--- leetcode/002.Add_Two_Numbers/Add_Two_Numbers_test.go
+++ leetcode/002.Add_Two_Numbers/Add_Two_Numbers_test.go
@@ -1,4 +1,29 @@
 package leetcode
 
+import (
+	"fmt"
+	"testing"
+)
+
 type question002 struct {
+	para2
+	ans2
+}
+
+type para2 struct {
+	one     []int
+	another []int
+}
+
+type ans struct {
+	one []int
+}
+
+func Test_Question2(t *testing.T) {
+	qs := []question002{}
+	fmt.Printf("Leetcode Problem 2\n")
+	for _, q := range qs {
+
+	}
+	fmt.Printf("\n\n\n")
 }
